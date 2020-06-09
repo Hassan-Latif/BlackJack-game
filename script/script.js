bfb01f99885a43134b5c8451a7789099ac1cde4f
Hassan Latif <ranashb520@gmail.com>
2020-06-09 16:43:42 +0500

    Part 3 of the javascript has been added

--- script/script.js
+++ script/script.js
@@ -2,7 +2,8 @@
 let blackJackGame = {
     'you' : {'scoreSpan':'#your-blackjack-result', 'div':'#your-box','score':0},
     'dealer' : {'scoreSpan':'#dealer-blackjack-result', 'div':'#dealer-box','score':0},
-    'cards' : ['2','3','4','5','6','7','8','9','10','A','K','A']
+    'cards' : ['2','3','4','5','6','7','8','9','10','A','K','J','Q'],
+    'cardsMap' : {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'K':10, 'Q':10 , 'A':[1,11]}
 }
 
 const YOU = blackJackGame['you'];
@@ -15,24 +16,33 @@ document.querySelector('#blackjack-hit-button').addEventListener("click", blackJ
 //for deal button
 document.querySelector('#blackjack-deal-button').addEventListener("click",blackJackDeal)
 
+//Function for Hit button
+
 function blackJackHit(){
-   let cards=randomCard();
-   console.log(cards);
-   
-    doShowCard(cards,Dealer);
+    let cards=randomCard();
+   console.log(cards);   
+    doShowCard(cards,YOU);
+    updateScore(cards,YOU);
+    showScore(YOU);
 }
-    
+
 function randomCard(){
     let randomNo = Math.floor(Math.random()*13);
     return blackJackGame.cards[randomNo];
 }
 
+//Function for showing images
+
 function doShowCard(card ,activePlayer){
+    if(activePlayer.score<=21){
     let cardImage = document.createElement('img');
     cardImage.src= `./images/${card}.png`;
     document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
 }
+}
+
+// Function for deal button
 
 function blackJackDeal(){
     let youImages=document.querySelector('#your-box').querySelectorAll('img');
@@ -47,5 +57,41 @@ function blackJackDeal(){
         youImages[i].remove();
     }
 
+    YOU.score=0;
+    Dealer.score=0;
+
+    document.querySelector('#your-blackjack-result').textContent =0;
+    document.querySelector('#dealer-blackjack-result').textContent =0;
+
+    document.querySelector('#your-blackjack-result').style.color='#ffffff';
+    document.querySelector('#dealer-blackjack-result').style.color='#ffffff'
 }
 
+//Update Function
+
+function updateScore(card,activePlayer){
+    if(card === 'A'){
+    if(activePlayer.score + blackJackGame.cardsMap[card][1] <=21){
+        activePlayer.score += blackJackGame.cardsMap[card][1];
+    }
+    else{
+        activePlayer.score +=blackJackGame.cardsMap[card][0];
+    }
+}
+else{
+    activePlayer.score += blackJackGame.cardsMap[card];   
+}
+}
+
+//Function to display Score
+
+function showScore(activePlayer){
+    if(activePlayer.score<=21){
+    document.querySelector(activePlayer['scoreSpan']).textContent= activePlayer.score;
+}
+    else{
+        document.querySelector(activePlayer.scoreSpan).textContent="Burst";
+        document.querySelector(activePlayer.scoreSpan).style.color='red';
+    }
+
+}
